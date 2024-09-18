88392ab25204a59418b2e2954af94e7594958f27
kydang1 <ky_dang@hotmail.com>
2024-09-18 10:59:04 -0400

    Update suppliestracker.py
    added the first basis of the code - able to grab from OSRS prices API and calculate Total Cost of Saradomin Brews used

--- suppliestracker.py
+++ suppliestracker.py
@@ -1,11 +1,24 @@
+import requests
+import json
+import math
+
+url = f"https://prices.runescape.wiki/api/v1/osrs/latest"
+
 def ItemPicker():
+    global url
     print ("Select an item\nSaradomin Brew(4)\nSuper Restore(4)\nSanfew Serum(4)\nDivine Super Combat(4)")
     answer = input("")
     if answer == ("Saradomin Brew(4)") or answer == ("brew") or answer == ("saradomin brew"):
-        print ("How many Saradomin Brews are you using?")
-        supplies = input("")
-    elif answer == ("Super Restore(4)") or answer == ("restore") or answer == ("super restore"):
-        print ("How many Super Restores are you using?")
-        supplies = input("")
- 
+        SaradominBrew4response = requests.get(url)
+        print (SaradominBrew4response)
+        if SaradominBrew4response.status_code == 200:
+            potionData = SaradominBrew4response.json()
+            SaradominBrew4Price = potionData["data"]["6685"]["high"]
+            print (SaradominBrew4Price)
+        print ("How many are you using?")
+        supplies = int(input(""))
+    TotalCost = SaradominBrew4Price * supplies
+    print ("Total Cost:")
+    print (TotalCost)
+
 ItemPicker()
